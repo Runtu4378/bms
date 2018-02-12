502122d81365a8f9beb42b16b084d646dd59c506
runtu <huorunquan@126.com>
2018-02-12 09:57:23 +0800

    加入样式初始化

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,25 @@ export default {
 </script>
 
 <style>
-#app {
+body {
+  font-size:14px;
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+/* 样式初始化 */
+body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,h6,pre,code,form,fieldset,legend,input,button,textarea,p,blockquote,th,td { margin:0; padding:0; }
+td,th,caption { font-size:14px; }
+h1, h2, h3, h4, h5, h6 { font-weight:normal; font-size:100%; }
+address, caption, cite, code, dfn, em, strong, th, var { font-style:normal; font-weight:normal;}
+a { color:#555; text-decoration:none; }
+a:hover { text-decoration:underline; }
+img { border:none; }
+ol,ul,li { list-style:none; }
+input, textarea, select, button { font:14px Verdana,Helvetica,Arial,sans-serif; }
+table { border-collapse:collapse; }
+html {overflow-y: scroll;}
+
+.clearfix:after {content: "."; display: block; height:0; clear:both; visibility: hidden;}
+.clearfix { *zoom:1; }
 </style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import utils from './common/utils'
 import Counter from './routes/index.vue'
 
 Vue.use(VueRouter)
@@ -7,12 +8,24 @@ Vue.use(VueRouter)
 const Foo = { template: '<div>foo</div>' }
 
 const routes = [
-  { path: '/', component: Foo },
+  {
+    path: '/',
+    title: '主页',
+    component: Foo,
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   routes,
+})
+
+router.afterEach(transition => {
+  router.options.routes.forEach(ele => {
+    if (ele.name === transition.name) {
+      utils.setTitle(ele.title)
+    }
+  })
 })
 
 export default router
